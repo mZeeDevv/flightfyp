2b535ab79ae68966b883477f912ad4dddc88f885
mZeeDevv <hamzeekhattak56@gmail.com>
2025-02-26 13:46:16 +0500

    Added packages

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { ToastContainer,toast } from 'react-toastify';
+import './index.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1
+        onClick={() => {
+          toast.success("HELLO WORLD")
+        }}
+        >HELLO </h1>
+      </div>
+      <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick={false}
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
+    </>
+    
+  )
+}
+export default App
